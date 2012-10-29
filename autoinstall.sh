551dc0d947cc4bd08414184dced39f71a0cecc68
Ruoran Liu <rrliu@thoughtworks.com>
2012-10-29 15:03:36 +0800

    Ruoran: detect if mysql command is available or provide warning

--- autoinstall.sh
+++ autoinstall.sh
@@ -6,6 +6,10 @@ if [ $# -ne 3 ]; then
   exit 1
 fi
 
+if ! which mysql > /dev/null; then
+  echo " Warning: Can't find mysql command. Please ensure you have manually set up clean database."
+fi
+
 drush make profiles/drupal_ci/drupal_ci.make -y --no-core &&
 drush si drupal_ci -y --account-name=$1 --account-pass=$2 --site-name="$3" &&
 ln -sfh `pwd` /Applications/XAMPP/xamppfiles/htdocs/drupal_ci
