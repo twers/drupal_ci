218b225eef7a3910de13befdbfe9054a548b3586
Ruoran Liu <rrliu@thoughtworks.com>
2012-10-29 15:02:49 +0800

    Ruoran: support spaces in sitename within script

--- autoinstall.sh
+++ autoinstall.sh
@@ -7,5 +7,5 @@ if [ $# -ne 3 ]; then
 fi
 
 drush make profiles/drupal_ci/drupal_ci.make -y --no-core &&
-drush si drupal_ci -y --account-name=$1 --account-pass=$2 --site-name=$3 &&
+drush si drupal_ci -y --account-name=$1 --account-pass=$2 --site-name="$3" &&
 ln -sfh `pwd` /Applications/XAMPP/xamppfiles/htdocs/drupal_ci
